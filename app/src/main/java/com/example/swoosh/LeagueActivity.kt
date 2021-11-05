e2f3c800234b713661340fa3b932e1471bbe16c6
umerasghar24 <uasghar24@gmail.com>
2021-11-05 10:36:17 +0500

    lesson 5

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,11 +1,24 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() { //we inherit from base activity so it will tell about activity life cycle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+    fun leagueNextClicked(vie:View){
+        val skillIntent =Intent(this,SkillActivity::class.java)
+        startActivity(skillIntent)
+
+    }
+    fun onMensClicked(view:View){
+
+    }
+    fun onWomensClicked(view:View){
+
     }
 }
